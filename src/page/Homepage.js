7f5cd43be410be4a62d68a0146150ef175ba2dd5
Silver <Silverg33k@gmail.com>
2022-11-30 16:11:53 +0100

    Added metamask connection

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import SectionOne from '../components/sectionOne/SectionOne'
+import Home from '../components/Home'
 
 function Homepage() {
   return (
     <div>
-        <SectionOne />
+        <Home />
     </div>
   )
 }
